7db6f010c1117db2797275397b3e3d7df36ad3a8
GCalizaya <giordanovcc3@gmail.com>
2022-06-29 17:07:19 -0500

    Version de la matriz terminada

--- TecladoMatriz/TMatriz/FTeclado.js
+++ TecladoMatriz/TMatriz/FTeclado.js
@@ -1,66 +1,57 @@
- 
-function init(){
-    //Variables
-    var resultado = document.getElementById("ipRes");
-    var uno = document.getElementById("num1");
-    var dos = document.getElementById("num2");
-    var tres = document.getElementById("num3");
-    var cuatro = document.getElementById("num4");
-    var cinco = document.getElementById("num5");
-    var seis = document.getElementById("num6");
-    var siete = document.getElementById("num7");
-    var ocho = document.getElementById("num8");
-    var nueve = document.getElementById("num9");
-    var cero = document.getElementById("num0");
-    var clearBtn = document.getElementById("btnBorrarTodo");
+/* ------------------------------------------------------------------------------------------------ */
+/*                              Funcion para obtener el output y para imprimir                      */
+/* ------------------------------------------------------------------------------------------------ */
+/* ------------------------------------------------------------------------------------------------ */
 
-    
-    
-    //Eventos
-
-    uno.onclick = function(e){
-        resultado.textContent = resultado.textContent + "1";
-    }
-    dos.onclick = function(e){
-        resultado.textContent = resultado.textContent + "2";
-    }
-    tres.onclick = function(e){
-        resultado.textContent = resultado.textContent + "3";
-    }
-    cuatro.onclick = function(e){
-        resultado.textContent = resultado.textContent + "4";
-    }
-    cinco.onclick = function(e){
-        resultado.textContent = resultado.textContent + "5";
-    }
-    seis.onclick = function(e){
-        resultado.textContent = resultado.textContent + "6";
-    }
-    siete.onclick = function(e){
-        resultado.textContent = resultado.textContent + "7";
-    }
-    ocho.onclick = function(e){
-        resultado.textContent = resultado.textContent + "8";
-    }
-    nueve.onclick = function(e){
-        resultado.textContent = resultado.textContent + "9";
-    }
-    cero.onclick = function(e){
-        resultado.textContent = resultado.textContent + "0";
-    }
-    
-    clearBtn.addEventListener('click', ()=> {
-        resultado.innerHTML = "";
-    }) 
-
-    
-    
-    
-    
-
-
-
-
+function getOutput(){
+	return document.getElementById("output-value").innerText;
 }
 
+function printOutput(num){
+    document.getElementById("output-value").innerText=num;
+}
+
+/* ------------------------------------------------------------------------------------------------ */
+/* ------------------------------------------------------------------------------------------------ */
+/* ------------------------------------------------------------------------------------------------ */
+/*                              Funcion para el backspace                                           */
+/* ------------------------------------------------------------------------------------------------ */
+/* ------------------------------------------------------------------------------------------------ */
+
+function reverseNumberFormat(num){
+	return Number(num.replace('0',''));
+}
+/* ------------------------------------------------------------------------------------------------ */
+/* ------------------------------------------------------------------------------------------------ */
+/*                              Bucle para clear y back                                             */
+/* ------------------------------------------------------------------------------------------------ */
+/* ------------------------------------------------------------------------------------------------ */
+
+var operator = document.getElementsByClassName("operator");
+for(var i =0;i<operator.length;i++){
+	operator[i].addEventListener('click',function(){
+		if(this.id=="clear"){
+			printOutput("");
+		}
+		else if(this.id=="backspace"){
+			var output=reverseNumberFormat(getOutput()).toString();
+			if(output){//if output has a value
+				output= output.substr(0,output.length-1);
+				printOutput(output);
+			}
+		}	
+	});
+}
+var number = document.getElementsByClassName("number");
+for(var i =0;i<number.length;i++){
+	number[i].addEventListener('click',function(){
+		var output=getOutput();
+		if(output!=NaN){ //if output is a number
+			output=output+this.id;
+			printOutput(output);
+		}
+	});
+}
+/* ------------------------------------------------------------------------------------------------ */
+/* ------------------------------------------------------------------------------------------------ */
 
