6630cec46fdf3b0f538139ed201fd01f88c01963
GCalizaya <giordanovcc3@gmail.com>
2022-08-18 23:47:47 -0500

    Se agrego la tabla y seleccion multiple en la interfaz reportes

---
+++ Reportes/V2Repo/JSSelect/RVselect.js
@@ -0,0 +1,33 @@
+
+
+const selectBtn = document.querySelector(".select-btn"),
+    items = document.querySelectorAll(".item");
+
+
+selectBtn.addEventListener("click", () => {
+    selectBtn.classList.toggle("open");
+});
+
+
+
+items.forEach(item => {
+    item.addEventListener("click", () => {
+        item.classList.toggle("checked");
+
+        let checked = document.querySelectorAll(".checked"),
+            btnText = document.querySelector(".btn-text");
+
+        if (checked && checked.length > 0) {
+            btnText.innerText = `${checked.length} Elementos seleccionados`;
+        } else {
+            btnText.innerText = "Seleccionar";
+        }
+
+    });
+})
+
+
+
+
+
+
